efa43730a761a5e2b9891d2d3d926f4d0d9d60cb
hisniaMuna <hisniamuna12@gmail.com>
2019-09-12 14:15:13 +0700

    Ternary

---
+++ script.js
@@ -0,0 +1,8 @@
+// if someone has more than 10 house
+// They are said 'Rich', otherwise
+// They are said 'poor'
+
+let house = 140;
+let type = house > 100 ? "Rich" : "Poor";
+
+console.log(type);
